3d9b9169c33d2ef38112799df0deb7e3b89c5c62
Max <moks.makarov@mail.ru>
2022-04-17 16:12:02 +0500

    commit 1

---
+++ ConsoleApp8/ConsoleApp8/Node.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+	public static class NodeExtensions
+	{
+		public static IEnumerable<Node> BreadthSearch(this Node startNode)
+		{
+			var visited = new HashSet<Node>();
+			var queue = new Queue<Node>();
+			queue.Enqueue(startNode);
+			while (queue.Count != 0)
+			{
+				var node = queue.Dequeue();
+				if (visited.Contains(node)) continue;
+				visited.Add(node);
+				yield return node;
+				foreach (var incidentNode in node.IncidentNodes)
+					queue.Enqueue(incidentNode);
+			}
+		}
+	}
+	public class Node
+	{
+		readonly List<Edge> edges = new List<Edge>();
+		public readonly int NodeNumber;
+
+		public Node(int number)
+		{
+			NodeNumber = number;
+		}
+
+		public IEnumerable<Node> IncidentNodes
+		{
+			get
+			{
+				return edges.Select(z => z.OtherNode(this));
+			}
+		}
+		public IEnumerable<Edge> IncidentEdges
+		{
+			get
+			{
+				foreach (var e in edges) yield return e;
+			}
+		}
+		public static Edge Connect(Node node1, Node node2, int value, Graph graph)
+		{
+			if (!graph.Nodes.Contains(node1) || !graph.Nodes.Contains(node2)) throw new ArgumentException();
+			var edge = new Edge(node1, node2, value);
+			node1.edges.Add(edge);
+			node2.edges.Add(edge);
+			return edge;
+		}
+		public static void Disconnect(Edge edge)
+		{
+			edge.From.edges.Remove(edge);
+			edge.To.edges.Remove(edge);
+		}
+	}
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ConsoleApp8
+{
+	class Program
+	{
+		public static List<Node> FindPath(Node start, Node end)
+		{
+			var track = new Dictionary<Node, Node>();
+			track[start] = null;
+			var queue = new Queue<Node>();
+			queue.Enqueue(start);
+			while (queue.Count != 0)
+			{
+				var node = queue.Dequeue();
+				var next = node.IncidentEdges.OrderBy(x => x.Value).First().OtherNode(node);
+				track[next] = node;
+				queue.Enqueue(next);
+				if (track.ContainsKey(end)) break;
+			}
+			var pathItem = end;
+			var result = new List<Node>();
+			while (pathItem != null)
+			{
+				result.Add(pathItem);
+				pathItem = track[pathItem];
+			}
+			result.Reverse();
+			return result;
+		}
+
+		public static void Main()
+		{
+			var graph = Graph.MakeGraph(
+				0, 1,
+				1, 2,
+				2, 3,
+				3, 4,
+				0, 3);
+			Node.Connect(graph.nodes[0], graph.nodes[3], 1, graph);
+			Node.Connect(graph.nodes[3], graph.nodes[4], 1, graph);
+			foreach (var elem in FindPath(graph.nodes[0],graph.nodes[4]))
+				Console.Write(elem.NodeNumber + " ");
+		}
+	}
+}
