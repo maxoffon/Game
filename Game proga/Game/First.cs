f14036d0aac25273e9d179d0584f003cdb3b7e76
Max <moks.makarov@mail.ru>
2022-05-03 23:48:04 +0500

    commit1

--- Game proga/Game/First.cs
+++ Game proga/Game/First.cs
@@ -21,46 +21,6 @@ namespace Game
             ClientSize = new Size(800, 600);
             BackgroundImage = Image.FromFile(@"C:\Users\НОРД\github\Game\Game proga\Game\Sprites\фон2.jpg");
             BackgroundImageLayout = ImageLayout.Stretch;
-            var a = new Planes(this);
-            a.Main();
         }
-    }
-
-    public class Planes
-    {
-        int planeNumber = 10;
-        public Form Form = new First();
-        public Planes(Form form)
-        {
-            Form = form;
-        }
-        public void Main()
-        {
-            var timer = new Timer();
-            timer.Interval = 1000;
-            timer.Tick += (sender, args) =>
-            {
-                MovePlane();
-                planeNumber--;
-                if (planeNumber == 0) return;
-            };
-            timer.Start();
-        }
-        public void MovePlane()
-        {
-            var plane = new Plane();
-            var picture = Menu.CreatePicture(plane.Current, new Size(100, 100), new Point(-100, 10));
-            while (picture.Location.X != Form.ClientSize.Height)
-            {
-                picture = CreateNew(picture, plane.Speed);
-                Form.Invalidate();
-            }
-        }
-
-        PictureBox CreateNew(PictureBox picture, int offset)
-        {
-            var newLocation = new Point(picture.Location.X + offset, picture.Location.Y);
-            return Menu.CreatePicture((string)picture.Tag, picture.Size, newLocation);
-        }
-    }
+    }  
 }
--- Game proga/Game/Plane.cs
+++ Game proga/Game/Plane.cs
@@ -12,25 +12,6 @@ namespace Game
 {
     class Plane
     {
-        public static readonly string[] Planes = new string[]
-        {
-            @"C:\Users\НОРД\github\Game\Game proga\Game\Sprites\самолёт.png",
-            @"C:\Users\НОРД\github\Game\Game proga\Game\Sprites\самолёт2.png"
-        };
-        public readonly int Speed = 10;
-        public readonly Image Look;
-        public readonly string Current;
-
-        public Plane()
-        {
-            var random = new Random().Next(0, 2);
-            Look = Image.FromFile(Planes[random]);
-            Current = Planes[random];
-        }
-
-        public static void DropTheBomb()
-        {
-
-        }
+        
     }
 }
